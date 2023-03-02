7f8222c39bce8474461f7957b0626ee4d6d6318c
trifork3 <rwilliams8183@gmail.com>
2023-03-01 20:10:13 -0500

    basic slot look

--- playground/src/lib/components/Slot.svelte
+++ playground/src/lib/components/Slot.svelte
@@ -3,21 +3,30 @@
     export let t1, t2;
     export let locations;
 
-    let use_location = (locations.length > 0);
-    let style = `width: ${width}`;
+    const use_location = (locations.length > 0);
+    const style = `width: ${width}`;
+
+    let selected = false;
+
+    function handleClick() {
+        selected = !selected;
+    }
 </script>
 
 <div class="card" style={ style }>
-    <div class="card-body">
-        <p>{ t1 } -- { t2 }</p>
+    <!-- start/end time -->
+    <div class="card-body" on:click={handleClick} on:keypress={handleClick}>
+        <h1>{ t1 } -- { t2 }</h1>
     </div>
 
+    <!-- list of available locations (if applicable) -->
     {#if use_location}
     <div class="hstack gap-3">
-        {#each locations as loc}
+        {#each locations as loc, i}
+        {@const loc_id = loc + i}
         <div class="form-check">
-            <input type="checkbox" class="form-check-input" id=""/>
-            <label for="" class="form-check-label">{loc}</label>
+            <input type="checkbox" class="form-check-input" id={loc_id} />
+            <label for={loc_id} class="form-check-label">{loc}</label>
         </div>
         {/each}
     </div>
--- playground/src/routes/+page.svelte
+++ playground/src/routes/+page.svelte
@@ -1,23 +1,32 @@
-<style>
-    .content {
-        margin: 0 auto;
-    }
-</style>
-
 <script>
-    import Slot from '$lib/components/Slot.svelte';
+    // list the time slots/locations from 'default.json' and process form data
 
-    let locs = Array('library', 'room', 'hallway');
+    import Slot from '$lib/components/Slot.svelte';
+    import data from '$lib/default.json';
 
     function handleSubmit(event) {
         console.log(event.target);
     }
 </script>
 
+<style>
+    .content {
+        margin-top: 10vh;
+        margin-left: 10vw;
+    }
+</style>
+
 <div class="content">
     <form on:submit|preventDefault={handleSubmit}>
-        <Slot t1={"00:00"} t2={"01:00"} locations={locs} />
+        <div class="vstack gap-2">
+        {#each data.slots as s}
+            <Slot   t1={s.t1} 
+                    t2={s.t2} 
+                    locations={data.locations} />
+        {/each}
+        </div>
 
+        <br />
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
 </div>
