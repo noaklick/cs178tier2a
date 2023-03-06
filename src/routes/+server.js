66ef951082180e01afb156f709c6ac68004e65bb
trifork3 <rwilliams8183@gmail.com>
2023-03-06 15:20:52 -0500

    moved project into root folder

---
+++ src/routes/+server.js
@@ -0,0 +1,19 @@
+import userdata from '$lib/user.json';
+import { json } from '@sveltejs/kit';
+import * as fs from 'fs';
+
+/** @type {import('./$types').RequestHandler} */
+export async function POST({ request }) {
+    const user = await request.json();
+
+    userdata.data.push(user);
+
+    fs.writeFileSync('src/lib/user.json', JSON.stringify(userdata), (err) => {
+        if (err) { console.log("Error: " + err); }
+    });
+
+    console.log("User data added. Current data: ");
+    console.log(userdata.data);
+
+    return json('OK');
+}
